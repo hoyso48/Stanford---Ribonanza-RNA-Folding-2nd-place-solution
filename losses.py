05821b3278d61af031e8991d32d50cab28081d95
hoyso48 <81813194+hoyso48@users.noreply.github.com>
2023-12-21 05:03:00 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -13,23 +13,23 @@ def mae_loss(pred,target,mask,weights=None):
     loss = loss[msk].mean()
     return loss, sample_size
 
-class MAE(object):
-    def __init__(self): 
-        self.reset()
+# class MAE(object):
+#     def __init__(self): 
+#         self.reset()
         
-    def reset(self): 
-        self.x,self.y = [],[]
+#     def reset(self): 
+#         self.x,self.y = [],[]
         
-    def accumulate(self, pred, y):
-        assert pred.size(0) == 1
-        assert y.size(0) == 1
-        x = pred[0]
-        y = y[0].clip(0,1)
-        self.x.append(x)
-        self.y.append(y)
+#     def accumulate(self, pred, y):
+#         assert pred.size(0) == 1
+#         assert y.size(0) == 1
+#         x = pred[0]
+#         y = y[0].clip(0,1)
+#         self.x.append(x)
+#         self.y.append(y)
 
-    def value(self):
-        x,y = torch.cat(self.x,0),torch.cat(self.y,0)
-        loss = F.l1_loss(x, y, reduction='none')
-        loss = loss[~torch.isnan(loss)].mean()
-        return loss
+#     def value(self):
+#         x,y = torch.cat(self.x,0),torch.cat(self.y,0)
+#         loss = F.l1_loss(x, y, reduction='none')
+#         loss = loss[~torch.isnan(loss)].mean()
+#         return loss
